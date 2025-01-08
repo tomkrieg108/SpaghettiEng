4fd9b86161488a666e0ab7769e1b7122ddf02a36
Tom Krieg <tomkrieg108@gmail.com>
2025-01-08 22:55:05 +0700

    Changed OpenGL version to 3.2 for Linux

--- src/SpaghettiEng/Core/Window.cpp
+++ src/SpaghettiEng/Core/Window.cpp
@@ -41,8 +41,8 @@ namespace Spg
       SPG_ERROR("GLFW initalisation failed");
       glfwTerminate();
     }
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 		glfwWindowHint(GLFW_MAXIMIZED, GL_TRUE);
